61ddd77221b50ed24d6e31c46b68a66ee3d3745f
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2023-02-01 23:48:27 -0300

    project creation for unit tests

---
+++ NerdStore/tests/NerdStore.Catalogo.Teste/ProdutoTeste.cs
@@ -0,0 +1,32 @@
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.DomainObjects;
+using System;
+using Xunit;
+
+namespace NerdStore.Catalogo.Teste
+{
+    public class ProdutoTeste
+    {
+        [Fact]
+        public void Produto_Validar_ValidacoesDevemRetornarExceptions()
+        {
+            // Arrange & Act & Assert
+
+            var ex = Assert.Throws<DomainException>(() =>
+                new Produto(string.Empty, "Descricao", false, 100, Guid.NewGuid(), DateTime.Now, "Imagem", new Dimensoes(1, 1, 1))
+            );
+
+            Assert.Equal("O campo Nome do produto não pode estar vazio", ex.Message);
+
+            ex = Assert.Throws<DomainException>(() =>
+                new Produto("Nome", string.Empty, false, 100, Guid.NewGuid(), DateTime.Now, "Imagem", new Dimensoes(1, 1, 1))
+            );
+
+            Assert.Equal("O campo Descricao do produto não pode estar vazio", ex.Message);
+
+            ex = Assert.Throws<DomainException>(() =>
+                new Produto("Nome", "Descricao", false, 0, Guid.NewGuid(), DateTime.Now, "Imagem", new Dimensoes(1, 1, 1))
+            );
+        }
+    }
+}
